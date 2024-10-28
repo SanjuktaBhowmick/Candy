db41fe855ae16a96d0145760153a697a373721dd
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2024-10-28 15:43:24 -0500

    Header backgound image size dynamic

--- src/components/Publications/Publications.js
+++ src/components/Publications/Publications.js
@@ -24,6 +24,7 @@ const Publications = () => {
                 {publication.doi}
               </a>
             </p>
+            <hr className="publication-divider" /> {/* Line division */}
           </li>
         ))}
       </ul>
