0a08d138ee02e855f91eb4f047416a4af15d6627
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2025-05-09 14:04:43 -0500

    Updates

---
+++ precache-manifest.86092464f63b96279433d10fc3f7bcad.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df2447629bc6be99afd8259beeb56ae",
+    "url": "/Candy/index.html"
+  },
+  {
+    "revision": "641b0f86e0fa47f29d3e",
+    "url": "/Candy/static/css/main.3fe361ce.chunk.css"
+  },
+  {
+    "revision": "eb6e8c99a29fcbfb172f",
+    "url": "/Candy/static/js/2.e053ec9e.chunk.js"
+  },
+  {
+    "revision": "6a2a939c6c472481a460",
+    "url": "/Candy/static/js/3.5a11cc32.chunk.js"
+  },
+  {
+    "revision": "641b0f86e0fa47f29d3e",
+    "url": "/Candy/static/js/main.7fb33768.chunk.js"
+  },
+  {
+    "revision": "d8afffb55d3cbb8e909e",
+    "url": "/Candy/static/js/runtime~main.9bd9c7aa.js"
+  },
+  {
+    "revision": "13bf4fec49b12a656fad8abcf86df02d",
+    "url": "/Candy/static/media/BoyanaNorris.13bf4fec.jpg"
+  },
+  {
+    "revision": "caa939939d078c8ecdc20cb4d665d900",
+    "url": "/Candy/static/media/Goal-1.caa93993.png"
+  },
+  {
+    "revision": "6fa7ab7a06f3fb61c2531658546196b8",
+    "url": "/Candy/static/media/Goal-2.1.6fa7ab7a.png"
+  },
+  {
+    "revision": "e4884316f495dcf352409b678b3eb2a6",
+    "url": "/Candy/static/media/Goal-2.2.e4884316.png"
+  },
+  {
+    "revision": "45da8eeee060d122c2f3a60a003c5805",
+    "url": "/Candy/static/media/GraphPropertyUpdate.45da8eee.png"
+  },
+  {
+    "revision": "eb2021ce66d1a166647c1281b3ef0cab",
+    "url": "/Candy/static/media/IntellectualMerit.eb2021ce.png"
+  },
+  {
+    "revision": "011354585298dea8e89f5733609143fb",
+    "url": "/Candy/static/media/PeterRalph.01135458.png"
+  },
+  {
+    "revision": "3ace75203d19a610870933f37b129da7",
+    "url": "/Candy/static/media/SajalDas.3ace7520.jpg"
+  },
+  {
+    "revision": "f82ebd6f03c22c4f72eefaf4f055bbc9",
+    "url": "/Candy/static/media/SamuelFrimpong.f82ebd6f.jpg"
+  },
+  {
+    "revision": "e41d946a2e4e57690fdf4c4ca7084657",
+    "url": "/Candy/static/media/SanjuktaBhowmick.e41d946a.jpg"
+  },
+  {
+    "revision": "f4f3d770f5a4d212da9834896bffc62d",
+    "url": "/Candy/static/media/bg.f4f3d770.png"
+  },
+  {
+    "revision": "ef64684f37fb362df3b54c5840d38c9c",
+    "url": "/Candy/static/media/graph2.ef64684f.jpg"
+  },
+  {
+    "revision": "b381ff7ce0b510c44fd84728edeab77f",
+    "url": "/Candy/static/media/logo_white.b381ff7c.png"
+  }
+]);
